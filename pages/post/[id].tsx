b542c05ceced5afdb63cc865460bf2857218d9f5
James Wingert <jameswingert1@gmail.com>
2022-06-22 18:57:10 -0700

    Changing ID

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -65,7 +65,7 @@ const Post = ({ post, user }) => {
           <ul role="list">
             <li
               key={post.id}
-              className="flex flex-col col-span-1 text-center bg-white rounded-lg divide-y divide-gray-200 shadow"
+              className="flex flex-col col-span-1 p-6 text-center bg-white rounded-lg divide-y divide-gray-200 shadow"
             >
               <div className="flex-col flex-1">
                 <h3 className="mt-6 text-sm font-medium text-gray-900">
