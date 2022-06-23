addbc2ff07a4f8665fed0048f013f1ca76ec3d81
James Wingert <jameswingert1@gmail.com>
2022-06-22 18:33:26 -0700

    Fixed header

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -12,25 +12,20 @@ const ThemeSwitcherButton = ({ ...rest }) => {
         themeMode === 'pastel' ? `Switch to dark mode` : `Switch to light mode`
       }
     >
-      <div className="items-center mx-auto">
-        <label className="swap swap-rotate">
-          <input
-            className="hidden "
-            type="checkbox"
-            id="swap"
-            onClick={toggleTheme}
-            {...rest}
-          />
-          {themeMode === 'pastel' ? (
-            <MoonIcon
-              className="w-8 h-8 cursor-pointer swap-on
+      {themeMode === 'pastel' ? (
+        <MoonIcon
+          onClick={toggleTheme}
+          {...rest}
+          className="w-8 h-8 cursor-pointer
           "
-            />
-          ) : (
-            <SunIcon className="w-8 h-8 cursor-pointer swap-off" />
-          )}
-        </label>
-      </div>
+        />
+      ) : (
+        <SunIcon
+          onClick={toggleTheme}
+          {...rest}
+          className="w-8 h-8 cursor-pointer"
+        />
+      )}
     </Tooltip>
   );
 };
