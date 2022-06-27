9a1d45beaeae8d5eea348052509468645414400f
James Wingert <jameswingert1@gmail.com>
2022-06-27 01:58:27 -0700

    Changing background

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -5,7 +5,7 @@ import Header from './Header';
 const Layout = ({ children }) => {
   return (
     <div>
-      <Header /> <div className="h-screen bg-base-100">{children}</div>
+      <Header /> <div className=" bg-base-100">{children}</div>
     </div>
   );
 };
